3d77c48471663c70a66d939a09f80931c91a353d
ampnic <nicabuca@gmail.com>
2024-11-05 16:39:31 -0500

    today's codewars

---
+++ 8 kyu/isItEven.js
@@ -0,0 +1,7 @@
+function testEven(n) {
+    n % 2
+      // return: true or false
+  }
+
+
+  testEven(111)
---
+++ 8 kyu/practice.js
@@ -0,0 +1,7 @@
+function filter_list(l) {
+    for(i=1; i<l.length; i++){
+        console.log(l)
+    }
+}
+
+filter_list([1,2,'a','b'])
