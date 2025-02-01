f948cc035a45a7303214df31e119a83586c29d13
ampnic <bri453274@gmail.com>
2025-02-01 13:09:29 -0500

    a second codewars

---
+++ 8 kyu/DNAtoRNA.js
@@ -0,0 +1,10 @@
+// GCAT => GCAU
+function DNAtoRNA(dna){
+    // loop through the characters
+    // replace with a U
+    // return new characters
+
+    console.log(dna.replaceAll('T','U'))
+}
+
+DNAtoRNA('TTTT')
